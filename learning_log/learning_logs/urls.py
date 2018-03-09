169eaf4e75efdcc1b051094016cc157eb1daca91
Roman Khripunov <r_khripunov@BY1-AL-20160296.local>
2018-03-09 15:42:55 +0300

    add create and edit function to project

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),                                          # Home page
     url(r'^topics/$', views.topics, name='topics'),                                 # All topics
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),                 # Requested topic
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),                        # Page of creating a new topic
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),      # Create new entry
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),   # Edit entry
 ]
