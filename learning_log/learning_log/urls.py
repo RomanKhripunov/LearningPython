4f77925c6a3b0f8f4e1beba46b6227e88f33a2a2
Roman Khripunov <r_khripunov@BY1-AL-20160296.local>
2018-03-09 23:11:52 +0300

    add user registration and login/logout

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import include, url
 urlpatterns = [
     # path(r'^admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
-    url(r'', include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs'))
+    url(r'^users/', include(('users.urls', 'users'), namespace='users')),
+    url(r'', include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs')),
 ]
