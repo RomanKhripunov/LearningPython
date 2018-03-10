6456f06e44589bb1042254898dd9220a4e10a320
Roman Khripunov <r_khripunov@BY1-AL-20160296.local>
2018-03-10 16:29:16 +0300

    make users permission

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -12,21 +13,26 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Выводит список всех тем"""
+    # topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все её записи"""
     topic = Topic.objects.get(id=topic_id)
+    # check_topic_owner(request, topic)
     entries = topic.entry_set.order_by("-date_added")
     context = {'topic': topic, 'entries': entries}
     return render(request, "learning_logs/topic.html", context)
 
 
+@login_required
 def new_topic(request):
     """Определяет новую тему"""
     if request.method != 'POST':
@@ -36,15 +42,19 @@ def new_topic(request):
         # Отправлены данные POST; обработать данные
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запись по конкретной теме"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # Данные не отправлялись, создается новая форма
@@ -61,10 +71,12 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # Исходный запрос; форма заполняется данными для текущей записи
@@ -76,3 +88,8 @@ def edit_entry(request, entry_id):
             return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def check_topic_owner(request, topic):
+    if request.user != topic.owner:
+        raise Http404
