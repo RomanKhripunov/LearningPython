e76d2a7eeb537ac54a2c8d611246f68a9824678b
Roman Khripunov <roma.khripunov@gmail.com>
2018-03-11 21:41:45 +0300

    Change 500 error to 400 with unknown topics

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
@@ -25,7 +25,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Выводит одну тему и все её записи"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # check_topic_owner(request, topic)
     entries = topic.entry_set.order_by("-date_added")
     context = {'topic': topic, 'entries': entries}
